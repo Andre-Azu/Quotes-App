bfd780b68b91d268cf4f6fe144b301e3c769f7f3
andre azu <andreazuaa@gmail.com>
2021-08-24 13:16:12 +0300

    add styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ZamzComponent } from './zamz/zamz.component';
 import { QuotedetailComponent } from './quotedetail/quotedetail.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { FormsModule } from '@angular/forms';
+import { UnderlineDirective } from './underline.directive';
 // import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
@@ -15,6 +16,7 @@ import { FormsModule } from '@angular/forms';
     ZamzComponent,
     QuotedetailComponent,
     QuoteFormComponent,
+    UnderlineDirective,
     // DateCountPipe
     
   ],
